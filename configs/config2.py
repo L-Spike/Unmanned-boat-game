2d83c10704d643bf4e6825098de02123cd4653cc
L-Spike <1186112845@qq.com>
2022-04-25 21:06:18 +0800

    add

---
+++ configs/config2.py
@@ -0,0 +1,33 @@
+cuda_device = "1"
+
+not_find_reward = -1
+done_dis = 1.4
+attack_num = 2
+attack_radius = 10
+defend_num = 6
+defend_radius = 2.5
+forbidden_radius = 1.5
+threat_angle = 45
+threat_angle_delta = 10
+small_angle = 10
+attack_threat_dis = 0.5
+defend_threat_dis = 4
+capture_dis = 1
+reward_agent_num = 2
+max_velocity = 5
+max_turn_angle = 5
+
+hidden_dim = 64
+max_step = 1500
+GAMMA = 0.99
+n_episode = 50000
+run_n_episode = 10
+i_episode = 0
+time_to_render = 1 / 240
+capacity = 200000
+batch_size = 128
+n_epoch = 25
+epsilon = 0.9
+score = 0
+lamb = 0.1
+tau = 0.98
