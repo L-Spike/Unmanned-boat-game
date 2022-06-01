dbefc49ea3a72040f8dfad2abc0d08b86ae7d573
L-Spike <1186112845@qq.com>
2022-06-01 14:46:51 +0800

    add

--- RLUtils.py
+++ RLUtils.py
@@ -75,7 +75,7 @@ class RolloutWorker:
         #         epsilon = epsilon - self.anneal_epsilon if epsilon > self.end_epsilon else epsilon
 
         step = 0
-        print("startXXXXXXXXXXXXx")
+        # print("startXXXXXXXXXXXXx")
         while not terminated and step < self.episode_limit:
             obs, _ = self.env.get_obs()
             state = self.env.get_state()
@@ -92,7 +92,7 @@ class RolloutWorker:
                 actions_onehot.append(action_onehot)
                 avail_actions.append(avail_action)
                 last_action[agent_id] = action_onehot
-            print("inXXXXXXXXXXXXx")
+            # print("inXXXXXXXXXXXXx")
             # print("actions: ", actions)
             reward, terminated, info = self.env.step(actions)
             reward = reward[0]
@@ -155,19 +155,20 @@ class RolloutWorker:
             terminated=terminate.copy()
         )
         for key in episode.keys():
-            try:
-                episode[key] = np.array([episode[key]])
-            except:
-                print("EXXX\n")
-                print(f"key:{key}")
-                print(len(episode[key]))
-                if isinstance(episode[key][0], list):
-                    print(len(episode[key][0]))
+            # try:
+            episode[key] = np.array([episode[key]])
+            # except:
+            #     print("EXXX\n")
+            #     print(f"key:{key}")
+            #     print(len(episode[key]))
+            #     if isinstance(episode[key][0], list):
+            #         print(len(episode[key][0]))
         if not evaluate:
             self.start_epsilon = epsilon
         if evaluate and episode_num == self.conf.evaluate_epoch - 1 and self.conf.replay_dir != '':
-            self.env.save_replay()
-            self.env.close()
+            # self.env.save_replay()
+            # self.env.close()
+            pass
 
         return episode, episode_reward, win_tag
 
